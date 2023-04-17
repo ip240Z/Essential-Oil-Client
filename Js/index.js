8b238c07df00b6ffc2992bfdaeaeafc24df26117
Ian <ip240z@protonmail.com>
2023-04-17 10:43:32 -0400

    Added the functionality of pulling data from the accessed api

--- Js/index.js
+++ Js/index.js
@@ -3,12 +3,21 @@ let basket = [];
 let userInfoArea = document.querySelector('#user-info')
 let oilInfoArea = document.querySelector('#oil-info')
 
+
 fetch(`http://localhost:3000/users`).then(r => {return r.json()}).then(data => {
-    console.log(data)    
+    let users = data  
+    console.log(data);  
+    let randUser = data[Math.floor(Math.random() * data.length)];
+    console.log(randUser);
+    let userNameP = document.createElement(`p`);
+    userNameP.innerText = `${randUser.last_name}, ${randUser.first_name}`;
+    userInfoArea.append(userNameP);
 })
 
 fetch(`http://localhost:3000/oils`).then(r => {return r.json()}).then(data => {
-    console.log(data)    
+    let oils = data;
+    console.log(data); 
+    let randOil = data[Math.floor(Math.random() * data.length)]; 
+    console.log(randOil);
+    oilInfoArea.insertAdjacentHTML('afterbegin', `<p>${randOil.name}</p> \n <p>${randOil.usage_methods}</p>`)
 })
-
-
